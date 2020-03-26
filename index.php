07062cd92a8865bb880f19ad916e04f700e32c77
omarsilord <48257471+omarsilord@users.noreply.github.com>
2020-03-26 17:44:37 -0600

    Iformacion de la pagina

--- index.php
+++ index.php
@@ -7,18 +7,65 @@ include 'Header.php';
       <div class="Inicio Centrar">
         <div class="Centar">
           <h1>Bienvenidos a ProblemRESULT</h1>
+          <h3>¿Cómo funciona nuestra aplicación?</h3>
+          <table>
+            <tr>
+              <td>
+                <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                <h5 class="card-title">¿Cómo iniciar?</h5>
+                <h6 class="card-subtitle mb-2 text-muted"></h6>
+                <p class="card-text">Solo selecciona la opción "Problemas" del menu que ves justo a tu izquierda, selecciona la categoria y el problema que mas te llame la atención!!!</p>
+                </div>
+                </div>
+              </td>
+              <td>
+                <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                <h5 class="card-title">¿Qué es lo que sigue?</h5>
+                <h6 class="card-subtitle mb-2 text-muted"></h6>
+                <p class="card-text">Se mostrará una interfaz en la que lo primero que tendrás que hacer es dar click en el botón de
+                  <button class="btn btn-primary" type="submit">
+                     Descripcion
+                </button>
+                 en donde se mostrará el contenido del problema</p>
+                </div>
+                </div>
+              </td>
+              <td>
+                <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                <h5 class="card-title">¿Y despues ?</h5>
+                <h6 class="card-subtitle mb-2 text-muted"></h6>
+                <p class="card-text">Una vez que analisaste el problema (MUY BIEN ANALIZADO :) ),
+                  deberás responder con los valores que satisfagan al problema en la parte de la izquierda, en las entradas de datos y dar cick en
+                  <button class="btn btn-primary" type="submit">
+                       Procesar
+                  </button></p>
+                </div>
+                </div>
+              </td>
+              <td>
+                <div class="card" style="width: 18rem;">
+                <div class="card-body">
+                <h5 class="card-title">Por Ultimo!!!!!!!</h5>
+                <h6 class="card-subtitle mb-2 text-muted"></h6>
+                <p class="card-text">Si al momento de procesar los datos que ingresaste están mal, no te sientas triste, para eso tenemos nuestro botón de
+                  <button class="btn btn-primary" type="submit">
+                       Soluciones
+                  </button>
+                  en dónde te daremos algunas soluciones, si sigues teniendo dudas pues <a href="#" class="card-link">comunicate</a> con nosotros
+                 </p>
 
-          <p>Nullam quis risus eget <a href="#">urna mollis ornare</a> vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula.</p>
-          <p><small>This line of text is meant to be treated as fine print.</small></p>
-          <p>The following is <strong>rendered as bold text</strong>.</p>
-          <p>The following is <em>rendered as italicized text</em>.</p>
-          <p>An abbreviation of the word attribute is <abbr title="attribute">attr</abbr>.</p>
-        </div>
-      </div>
+                </div>
+                </div>
+              </td>
+            </tr>
+          </table>
 
 
     <div class="Pie">
-?
+?>
     </div>
 
 
