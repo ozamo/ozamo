532e271fdf87f4295dc318967af5d3458f2a0c7a
JLRN10 <62582461+JLRN10@users.noreply.github.com>
2020-03-25 19:55:55 -0600

    creacion

---
+++ index.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title></title>
+  <link rel="stylesheet" type="text/css" href="CSS/bootstrap.css">
+</head>
+<body>
+  <div class="Principal">
+    <div class="Menu">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+        <a class="navbar-brand" href="#"><img src="Imagenes/Logos.png" alt="Imagen rota" width=50% ></a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor01">
+          <ul class="navbar-nav mr-auto">
+            <li class="nav-item active">
+              <h1><a class="nav-link" href="#">Problemas <span class="sr-only">(current)</span></a></h1>
+            </li>
+            <li class="nav-item">
+              <h1><a class="nav-link" href="#">About</a></h1>
+            </li>
+          </ul>
+          <form class="form-inline my-2 my-lg-0">
+            <input class="form-control mr-sm-2" type="text" placeholder="Search">
+            <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
+          </form>
+        </div>
+      </nav>
+    </div>
+    <div class="Contenido">
+      <div class="BarraDeBusqueda">
+        Menu
+      </div>
+
+      <div class="Programa">
+        <form>
+            <fieldset>
+              <h1><legend>Legend</legend></h1>
+              <div class="form-group">
+                <label class="col-form-label" for="inputDefault">Default input</label>
+                <input type="text" class="form-control" placeholder="Default input" id="inputDefault">
+              </div>
+              <div class="form-group">
+                <label class="col-form-label" for="inputDefault">Default input</label>
+                <input type="text" class="form-control" placeholder="Default input" id="inputDefault">
+              </div>
+
+
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </fieldset>
+
+          </form>
+
+      </div>
+      <div class="AyudaPrograma">
+       <form>
+            <fieldset>
+              <h1><legend>Legend</legend></h1>
+              <div class="form-group">
+                <label class="col-form-label" for="inputDefault">Default input</label>
+                <input type="text" class="form-control" placeholder="Default input" id="inputDefault">
+              </div>
+              <div class="form-group">
+                <label class="col-form-label" for="inputDefault">Default input</label>
+                <input type="text" class="form-control" placeholder="Default input" id="inputDefault">
+              </div>
+               <div class="form-group">
+                <label for="exampleTextarea">Example textarea</label>
+                <textarea class="form-control" id="exampleTextarea" rows="3"></textarea>
+              </div>
+
+
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </fieldset>
+
+          </form>
+    </div>
+
+    <div class="Pie">
+?
+    </div>
+
+
+  </div>
+</body>
+</html>
