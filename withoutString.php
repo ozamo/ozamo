9443eb603c6503f15a32f673ac7c3889b5b0c35a
Norberto <17030711@itcelaya.edu.mx>
2020-03-27 17:05:51 -0600

    Creacion de Clases de los Problrmas 1,2,4

--- withoutString.php
+++ withoutString.php
@@ -1,12 +1,12 @@
 <?php
 
   include ('Header.php');
-  include ('Programas/inOrderEqual.php');
+  include ('Programas/withoutString.php');
 
 ?>
 <div class="Programa">
 
-     <form name="formaction" action="inOrderEqual.php">
+     <form name="formaction" action="withoutString.php">
           <fieldset>
                <h1>
                     <legend>
@@ -15,43 +15,26 @@
                </h1>
                <div class="form-group">
                     <label class="col-form-label" for="inputDefault">
-                         Entrada de Datos 1
+                         Cadena Base:
                     </label>
                     <input class="form-control" value=0 name="valor1" placeholder="Default input" type="text">
                     </input>
                </div>
                <div class="form-group">
                     <label class="col-form-label" for="inputDefault">
-                         Entrada de Datos 2
+                         Cadena a remover:
                     </label>
                     <input class="form-control" value=0 name="valor2" placeholder="Default input" type="text">
                     </input>
                   </div>
-                    <div class="form-group">
-                         <label class="col-form-label" for="inputDefault">
-                              Entrada de Datos 3
-                         </label>
-                         <input class="form-control"value=0 name="valor3" placeholder="Default input" type="text">
-                         </input>
-                    </div>
-                    <div class="form-group">
-                         <label class="col-form-label" for="inputDefault">
-                              Bandera
-                         </label>
-                         <input class="form-control" value='false' name="bandera" placeholder="Default input" type="text">
-                         </input>
-
-              </div>
               <div>
               <?php
               $Result=' ';
 
-                if(isset($_GET['valor1']) && isset($_GET['valor2']) && isset($_GET['valor3']) && isset($_GET['bandera'])){
+                if(isset($_GET['valor1']) && isset($_GET['valor2']))){
                   $valor1=$_GET['valor1'];
                   $valor2=$_GET['valor2'];
-                  $valor3=$_GET['valor3'];
-                  $bandera=$_GET['bandera'];
-                  $Result=$objeIOE->probInOrderEqual($valor1,$valor2,$valor3,$bandera);
+                  $Result=$objeWS->probWithoutString($valor1,$valor2);
                 }
               ?>
               <button class="btn btn-primary" type="post">
@@ -67,7 +50,7 @@
 
           </fieldset>
      </form>
-</div>
+   </div>
 <div class="AyudaPrograma">
 
 </div>
