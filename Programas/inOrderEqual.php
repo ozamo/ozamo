952591b4cf23e5b947f37374db3d00c8dbd2114d
Norberto <17030711@itcelaya.edu.mx>
2020-03-27 00:15:19 -0600

    Actualizacion de Programas 1,2,4

---
+++ Programas/inOrderEqual.php
@@ -0,0 +1,25 @@
+<?php
+//Problema No. 1 inOrderEqual
+
+	class inOrderEqual
+	{
+		public function inOrderEqual()
+		{
+			# code...
+		}
+
+		public function probInOrderEqual($valorA, $valorB, $valorC, $bandera)
+		{
+			if(!$bandera && $valorA < $valorB && $valorB < $valorC)
+				return "verdadero";
+
+			if($bandera && $valorA <= $valorB && $valorB <= $valorC)
+				return "verdadero";
+			else
+				return "falso";
+		}
+	}
+
+	//Creacion de Objeto para la demas clases
+	$objeIOE = new inOrderEqual();
+?>
