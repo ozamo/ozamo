f519125d7e7ac462d2ca45439105c892329e2bd6
omarsilord <48257471+omarsilord@users.noreply.github.com>
2020-03-31 15:52:11 -0600

    problemas

--- fix34.php
+++ fix34.php
@@ -1,50 +1,80 @@
 <?php
-
-  include ('Header.php');
   include ('Programas/fix34.php');
-
+  include ('Header.php');
 ?>
-<div class="Programaa">
 
-     <form name="formaction" action="fix34.php?E=1">
+<div class="Programaa">
+     <form name="formaction " action="fix34.php">
           <fieldset>
-               <h1>
+               <h1 class="CentrarItems">
                     <legend>
                          Programa
                     </legend>
                </h1>
-               <div class="form-group">
+               <div class="form-group CentarItemsDiv">
                     <label class="col-form-label" for="inputDefault">
                          Entrada de Datos 1
                     </label>
-                    <input class="form-control" value=0 name="valor1" placeholder="Default input" type="text">
+
+                    <input class="form-control " value=0 name="valor1" placeholder="Default input" type="text">
                     </input>
                </div>
 
+              <div>
               <?php
               $Result=' ';
 
-                if(isset($_GET['valor1'])){
+                if(isset($_GET['valor1']) ){
                   $valor1=$_GET['valor1'];
+
                   $Result=$objefx->profix34($valor1);
                 }
-              ?><button class="btn btn-primary" type="post">
-                   Procesar
-              </button>
-               <br><legend>
-                 <?php
-                   
-                   echo $Result;
-
-                 ?>
-                </legend>
+              ?>
+              <div class="CentarItemsDiv">
+                <button class="btn btn-primary CentarItems" type="post">
+                     Procesar
+                </button>
+              </div>
+               <br>
+               <div class="CentarItemsDiv2">
+                 <div class="card border-light mb-3 " style="max-width: 20rem;">
+                    <div class="card-header">Resultados</div>
+                      <div class="card-body">
+                          <h4 class="card-title"> <?php echo $Result;?></h4>
+                      </div>
+                </div>
+               </div>
 
           </fieldset>
      </form>
 </div>
 <div class="AyudaPrograma">
+  <form action="fix34.phpE=?" name='generico'>
+          <fieldset>
 
+               <div class="form-group">
+                    <label for="exampleTextarea">
+                         Area de texto de Descripcion y Soluciones
+                    </label>
+
+                    <div class="card border-light mb-3" style="max-width: 20rem;">
+                      <div class="card-header">Descripcion</div>
+                      <div class="card-body">
+                        <h4 class="card-title">Light card title</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      </div>
+                    </div>
+                    <br>
+                    <div class="card border-light mb-3" style="max-width: 20rem;">
+                      <div class="card-header">Posibles Soluciones</div>
+                      <div class="card-body">
+                        <h4 class="card-title">Light card title</h4>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                      </div>
+                    </div>
+               </div>
+          </fieldset>
+     </form>
 </div>
 <div class="Pie">
-
 </div>
